45b24fd76ea011fbcea0e840038c5be5ea6c459b
kenny <kennycs@protonmail.com>
2021-06-03 12:26:40 -0400

    Added check_proxy() function

--- api/scraper.py
+++ api/scraper.py
@@ -1,7 +1,20 @@
 import asyncio
 import aiohttp
 
+# maximum amount of milliseconds a proxy is allowed to take to connect
+proxy_timeout = 5000
 
+# returns the ip of a proxy; if 
+async def check_proxy(authentication_ip, username, password):
+    try:
+        async with aiohttp.ClientSession() as session:
+            proxy = 'http://' + username + ':' + password + '@' + authentication_ip
+            async with session.get('https://check-host.net/ip', proxy=proxy) as response:
+                ip = await(response.text)
+                return ip
+    except Exception:
+        return
+    
 
 # automatically rotate through valid proxies
 async def request():
