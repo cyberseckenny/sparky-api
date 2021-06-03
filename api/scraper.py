cd64743951ad78d7642f45f54da2ad7f0cd236db
kenny <kennycs@protonmail.com>
2021-06-03 12:53:24 -0400

    Added variable that defines the location of the proxy file

--- api/scraper.py
+++ api/scraper.py
@@ -6,6 +6,8 @@ import aiohttp
 
 # maximum amount of milliseconds a proxy is allowed to take to connect
 proxy_timeout = 5000
+# location of proxy file
+proxy_location = 'proxies'
 
 # returns the ip of a proxy; if 
 async def check_proxy(authentication_ip, username, password):
@@ -20,7 +22,7 @@ async def check_proxy(authentication_ip, username, password):
         return
 
 async def check_proxies():
-    proxies = open('proxies', 'r')
+    proxies = open(proxy_location, 'r')
     lines = proxies.readlines()
     # gets amount of lines in proxies file
     proxies_line_count = sum(1 for _ in proxies) 
