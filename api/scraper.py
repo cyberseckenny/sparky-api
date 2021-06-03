1f136e5a1feb55708a65fe6c26bc261c0570bc42
kenny <kennycs@protonmail.com>
2021-06-03 12:40:18 -0400

    Implemented timeouts to check_proxy

--- api/scraper.py
+++ api/scraper.py
@@ -10,14 +10,14 @@ proxy_timeout = 5000
 # returns the ip of a proxy; if 
 async def check_proxy(authentication_ip, username, password):
     try:
-        async with aiohttp.ClientSession() as session:
+        session_timeout = aiohttp.ClientTimeout(total=None, sock_connect=proxy_timeout,sock_read=timeout_seconds)
+        async with aiohttp.ClientSession(timeout=session_timeout) as session:
             proxy = 'http://' + username + ':' + password + '@' + authentication_ip
             async with session.get('https://check-host.net/ip', proxy=proxy) as response:
                 ip = await(response.text)
                 return ip
     except Exception:
         return
-    
 
 # automatically rotate through valid proxies
 async def request():
