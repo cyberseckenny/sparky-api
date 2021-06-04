b901745fd42ead70d6defac2be34ff40af99abc8
kenny <kennycs@protonmail.com>
2021-06-04 09:41:22 -0400

    Made DNT json value a string

--- api/scraper.py
+++ api/scraper.py
@@ -80,7 +80,7 @@ async def get_request(proxies, url):
                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                'Accept-Language': 'en-US,en;q=0.5',
                'Accept-Encoding': 'gzip, deflate, br',
-               'DNT': 1,
+               'DNT': '1',
                'Connection': 'keep-alive'}
 
     try: 
