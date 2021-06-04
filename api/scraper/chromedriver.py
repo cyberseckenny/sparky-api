ae7d3c4f11715087efc744078a620706959f1cea
kenny <kennycs@protonmail.com>
2021-06-04 15:09:09 -0400

    Implemented manfiest_json and background_js

--- api/scraper/chromedriver.py
+++ api/scraper/chromedriver.py
@@ -1,11 +1,65 @@
 import undetected_chromedriver as uc
 
+
+
 class Driver:
     def __init__(proxy_host, proxy_port, proxy_user, proxy_password):
         self.proxy_host = proxy_host
         self.proxy_port = proxy_port
         self.proxy_user = proxy_user
         self.proxy_password = proxy_password
+
+        manifest_json = """
+        {
+        "version": "1.0.0",
+                "manifest_version": 2,
+                "name": "Chrome Proxy",
+                "permissions": [
+                        "proxy",
+                "tabs",
+                "unlimitedStorage",
+                "storage",
+                "<all_urls>",
+                "webRequest",
+                "webRequestBlocking"
+                ],
+                "background": {
+                        "scripts": ["background.js"]
+                },
+                "minimum_chrome_version":"22.0.0"
+        }
+        """
+
+        background_js = """
+        var config = {
+            mode: "fixed_servers",
+            rules: {
+                singleProxy: {
+                    scheme: "http",
+                    host: "%s",
+                    port: parseInt(%s)
+                },
+                bypassList: ["localhost"]
+            }
+        };
+
+        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+        function callbackFn(details) {
+            return {
+                authCredentials: {
+                    username: "%s",
+                    password: "%s"
+                }
+            };
+        }
+
+        chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+        );
+        """ % (proxy_host, proxy_port, proxy_user, proxy_password)
    
     def get_chromedriver():
         path = os.path.dirname(os.path.abspath(__file__))
