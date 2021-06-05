ff33677db830aed3a4e09807024a982c56dc150c
kenny <kennycs@protonmail.com>
2021-06-05 12:48:14 -0400

    Completely removed proxies

--- api/scraper/driver_helper.py
+++ api/scraper/driver_helper.py
@@ -2,86 +2,30 @@ import undetected_chromedriver as uc
 import os
 import zipfile
 
-class Driver:
-    def __init__(self, proxy_host, proxy_port, proxy_username, proxy_password):
-        self.proxy_host = proxy_host
-        self.proxy_port = proxy_port
-        self.proxy_username = proxy_username
-        self.proxy_password = proxy_password
-
-        self.manifest_json = """
-        {
-        "version": "1.0.0",
-                "manifest_version": 2,
-                "name": "Chrome Proxy",
-                "permissions": [
-                        "proxy",
-                "tabs",
-                "unlimitedStorage",
-                "storage",
-                "<all_urls>",
-                "webRequest",
-                "webRequestBlocking"
-                ],
-                "background": {
-                        "scripts": ["background.js"]
-                },
-                "minimum_chrome_version":"22.0.0"
-        }
-        """
-
-        self.background_js = """
-        var config = {
-            mode: "fixed_servers",
-            rules: {
-                singleProxy: {
-                    scheme: "http",
-                    host: "%s",
-                    port: parseInt(%s)
-                },
-                bypassList: ["localhost"]
-            }
-        };
-
-        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
-
-        function callbackFn(details) {
-            return {
-                authCredentials: {
-                    username: "%s",
-                    password: "%s"
-                }
-            };
-        }
-
-        chrome.webRequest.onAuthRequired.addListener(
-            callbackFn,
-            {urls: ["<all_urls>"]},
-            ['blocking']
-        );
-        """ % (proxy_host, proxy_port, proxy_username, proxy_password)
-   
-    def get_chromedriver(self):
-        chrome_options = uc.ChromeOptions()
+def get_chromedriver():
+    chrome_options = uc.ChromeOptions()
     
-        # executable and binary locations
-        # TODO: don't hardcode this
-        chrome_options.binary_location = '/home/kenny/Builds/google-chrome/pkg/google-chrome/usr/bin/google-chrome-stable'        
+    # executable and binary locations
+    # TODO: don't hardcode this
+    chrome_options.binary_location = '/home/kenny/Builds/google-chrome/pkg/google-chrome/usr/bin/google-chrome-stable'        
 
-        # proxy stuff
-        # plugin_file = 'proxy_auth_plugin.zip'
-        # with zipfile.ZipFile(plugin_file, 'w') as zip:
-        #     zip.writestr('manifest.json', self.manifest_json)
-        #     zip.writestr('background.js', self.background_js)
-        # chrome_options.add_extension(plugin_file)    
+    # proxy stuff
+    # plugin_file = 'proxy_auth_plugin.zip'
+    # with zipfile.ZipFile(plugin_file, 'w') as zip:
+    #     zip.writestr('manifest.json', self.manifest_json)
+    #     zip.writestr('background.js', self.background_js)
+    # chrome_options.add_extension(plugin_file)    
     
-        prefs = {'profile.managed_default_content_settings.images': 2,
-                 'profile.managed_default_content_settings.javascript': 2,
-                 'profile.managed_default_content_settings.stylesheet': 2,
-                 'profile.managed_default_content_settings.css': 2}
-        chrome_options.add_experimental_option('prefs', prefs)
+    chrome_options.headless = True
+    chrome_options.add_argument('--headless')
 
-        # TODO: don't hardcode this
-        driver = uc.Chrome(executable_path='/home/kenny/Builds/chromedriver/src/chromedriver',
-                           chrome_options=chrome_options)
-        return driver
+    prefs = {'profile.managed_default_content_settings.images': 2,
+             'profile.managed_default_content_settings.javascript': 2,
+             'profile.managed_default_content_settings.stylesheet': 2,
+             'profile.managed_default_content_settings.css': 2}
+    chrome_options.add_experimental_option('prefs', prefs)
+
+    # TODO: don't hardcode this
+    driver = uc.Chrome(executable_path='/home/kenny/Builds/chromedriver/src/chromedriver',
+                       chrome_options=chrome_options)
+    return driver
--- api/scraper/scraper.py
+++ api/scraper/scraper.py
@@ -8,85 +8,9 @@ from datetime import datetime, timezone
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 
-# TODO: before performing a complete scrape of NameMC, check the upcoming names to ensure that you have enough time to do it.
-#       otherwise, you may miss out on some names, b/c NameMC will update and your offsets will be incorrect
-
-# maximum amount of seconds a proxy is allowed to take to connect
-proxy_timeout = 15
-# location of proxy file
-proxy_location = 'proxies'
-
-# stores a proxy ip as well as its credentials
-class Proxy:
-    def __init__(self, authentication_proxy, proxy_host, proxy_port, proxy_username, proxy_password, ip):
-        self.authentication_proxy = authentication_proxy
-        self.proxy_host = proxy_host
-        self.proxy_port = proxy_port
-        self.proxy_username = proxy_username
-        self.proxy_password = proxy_password
-        self.ip = ip
-
-# returns the ip and authentication ip of a proxy; authentication ip is the ip used to login into the proxy 
-async def check_proxy(authentication_ip, username, password):
-    try:
-        client_timeout = aiohttp.ClientTimeout(total=proxy_timeout)
-        async with aiohttp.ClientSession(timeout=client_timeout) as session:
-            proxy = 'http://' + username + ':' + password + '@' + authentication_ip
-            async with session.get('https://check-host.net/ip', proxy=proxy) as response:
-                ip = await(response.text())
-                return [ip, proxy]
-    except Exception:
-        return None
-
-async def check_proxies():
-    proxies_file = open(proxy_location, 'r')
-    lines = proxies_file.readlines()
-    proxies_line_count = len(lines) 
-    proxies_file.close()
-    valid_proxies = []
-
-    async def check(i):
-        line = lines[i].strip()
-        split = line.split(':')
-
-        authentication_ip = split[0] + ':' + split[1]
-        username = split[2]
-        password = split[3]
-        proxy = await check_proxy(authentication_ip, username, password)
-        if proxy is not None:
-            valid_proxies.append(Proxy(proxy[1], split[0], split[1], split[2], split[3], proxy[0]))
-
-    coroutines = [check(i) for i in range(proxies_line_count)]
-    await asyncio.gather(*coroutines)
-    
-    # loops through all proxies and removes duplicate
-    for x in valid_proxies:
-        for y in valid_proxies:
-            if x == y: # important check to make sure we don't remove every proxy
-                continue
-            if x.ip == y.ip:
-                valid_proxies = remove_proxy(valid_proxies, y)
-
-    return valid_proxies
-
-# removes a proxy from a proxy array and returns the modified version
-def remove_proxy(proxies, to_remove):
-    for i in range(0, len(proxies)):
-        proxy = proxies[i]
-        if proxy == to_remove:
-            proxies.pop(i)
-            break
-
-    return proxies
-
-# automatically rotate throughs valid proxies and does a get request
-def get_request(proxies, url):
-    proxy = proxies[0]
-    popped_proxy = proxies.pop(0)
-    proxies.append(popped_proxy)
-     
-    chrome_driver = driver_helper.Driver(proxy.proxy_host, proxy.proxy_port, proxy.proxy_username, proxy.proxy_password)
-    driver = chrome_driver.get_chromedriver()
+# uses chrome driver to scrape the elements of a page 
+def get_request(url):
+    driver = driver_helper.get_chromedriver()
 
     with driver:
         driver.get(url)
@@ -99,9 +23,9 @@ def get_request(proxies, url):
 def parse(html):
     return BeautifulSoup(html, 'html.parser')
     
-def scrape_name_mc(proxies):
-    for i in range(0, 1):
-        soup = get_request(proxies, 'https://namemc.com/minecraft-names')
+def scrape_name_mc():
+    for i in range(0, 1000):
+        soup = get_request('https://namemc.com/minecraft-names')
         name_containers = soup.find_all('div', class_ = re.compile('^row no-gutters py-1 px-3'))
         for name in name_containers:    
             player_name = name.find('a').text
@@ -123,18 +47,8 @@ def parse_time(drop_time):
     return unix_time
 
 async def main():
-    print('Checking for valid proxies...')
-    # proxies = await check_proxies() 
-    # proxies_len = len(proxies)
-    proxies_len = 5
-    proxies = [Proxy('lol', 'xd', 'im', 'fake', '1', '2')]
-    
-    print('Found ' + str(proxies_len) + ' valid proxies')
-    if (proxies_len > 0): 
-        print('Scraping NameMC...')
-        scrape_name_mc(proxies)
-    else:
-        return
+    print('Scraping NameMC...')
+    scrape_name_mc()
     
 if __name__ == '__main__':
     uvloop.install()
