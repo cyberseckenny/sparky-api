affdbe3f69c68c46644bbf6276b077fdcb039728
kenny <kennycs@protonmail.com>
2021-06-05 13:16:17 -0400

    Removed all validation related variables and methods

--- api/__init__.py
+++ api/__init__.py
@@ -11,9 +11,6 @@ from setuptools import setup
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-devicesSection = config['DEVICES']
-maximumBasicDevices = int(devicesSection['BASIC'])
-maximumPremiumDevices = int(devicesSection['PREMIUM'])
 mongoSection = config['MONGO']
 mongoIP = mongoSection['IP']
 ratelimitSection = config['RATELIMIT']
@@ -28,41 +25,6 @@ sentry_sdk.init(
 )
 
 mongo_client = MongoClient('mongodb://' + mongoIP + ':27017')
-db = mongo_client.license
-keys = db.keys
-
-def getDocumentFromKey(key):
-    for document in keys.find():
-        if document['key'] == key:
-            return document
-    app.logger.info("Key not found")
-    return None
-
-def getDataFromKey(key):
-    if keys.count_documents({}) > 0:
-        for document in keys.find():
-            if document['key'] == key:
-                devices = document['devices']
-                membership = document['membership']
-                return {'devices': devices, 'membership': membership}
-                break
-        app.logger.info("No data found from specified key")
-        return None
-    else:
-        app.logger.info("No keys found in database")
-        return None
-
-def addDevice(key):
-    document = getDocumentFromKey(key) 
-    id = document['_id']
-    devices = int(document['devices'])
-    jsonId = {"_id": id}
-    updatedValues = {"$set": {"devices": devices + 1}}
-    keys.update_one(jsonId, updatedValues)
-
-class PaymentRequired(HTTPException):
-    code = 402
-    description = 'Payment required.'
 
 # we import other files down here to prevent circular imports
 app = Flask(__name__)
@@ -71,7 +33,6 @@ limiter = Limiter(
     key_func=get_remote_address,
     default_limits=[maxRequestsPerMinute + " per minute"]
 )
-import api.validate_endpoint
 
 @app.errorhandler(HTTPException)
 def handle_exception(e):
@@ -80,9 +41,6 @@ def handle_exception(e):
     
     if e.code == 400:
         error = "MALFORMED_REQUEST"
-    elif e.code == 401:
-        error = "INVALID_KEY"
-    elif e.code == 402:
         error = "MAXIMUM_DEVICES"
     elif e.code == 404:
         # return render_template('page_not_found.html') 
@@ -105,8 +63,6 @@ def post_root():
     return render_template('root.html') 
 
 app.register_error_handler(400, handle_exception)
-app.register_error_handler(401, handle_exception)
-app.register_error_handler(PaymentRequired, handle_exception)
-app.register_error_handler(400, handle_exception)
+app.register_error_handler(404, handle_exception)
 app.register_error_handler(429, handle_exception)
 app.register_error_handler(500, handle_exception)
