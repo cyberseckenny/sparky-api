7d1b4509fc5e9539500e0b8739cbb04d7372ded5
kenny <kennycs@protonmail.com>
2021-06-05 12:55:02 -0400

    Removed proxy comments

--- api/scraper/scraper.py
+++ api/scraper/scraper.py
@@ -53,14 +53,6 @@ def get_chromedriver():
     # executable and binary locations
     # TODO: don't hardcode this
     chrome_options.binary_location = '/home/kenny/Builds/google-chrome/pkg/google-chrome/usr/bin/google-chrome-stable'        
-
-    # proxy stuff
-    # plugin_file = 'proxy_auth_plugin.zip'
-    # with zipfile.ZipFile(plugin_file, 'w') as zip:
-    #     zip.writestr('manifest.json', self.manifest_json)
-    #     zip.writestr('background.js', self.background_js)
-    # chrome_options.add_extension(plugin_file)    
-    
     chrome_options.headless = True
     chrome_options.add_argument('--headless')
 
