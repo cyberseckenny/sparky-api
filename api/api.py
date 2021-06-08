3840b9a2796a452367fece01fca7f26e4cf3fa44
kenny <kennycs@protonmail.com>
2021-06-08 19:42:00 -0400

    Corrected scraper count

--- api/api.py
+++ api/api.py
@@ -37,11 +37,11 @@ def addUpcomingNames(json_data, three, i):
     if three:
         UPCOMING_THREE.drop()
         UPCOMING_THREE.insert_many(data)
-        print('[' + str(i + 1) + '] ' + 'Updated upcoming three letter names at ' + str(now))
+        print('[' + str(i) + '] ' + 'Updated upcoming three letter names at ' + str(now))
     else:
         UPCOMING.drop()
         UPCOMING.insert_many(data)
-        print('[' + str(i + 1) + '] ' + 'Updated upcoming names at ' + str(now))
+        print('[' + str(i) + '] ' + 'Updated upcoming names at ' + str(now))
 
 # the 'three' boolean indicates if the json_data is three letter names
 def getUpcomingNames(three):
